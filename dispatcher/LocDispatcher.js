762a02fd5642683c928a5b958c8f9657a783746b
LaurenCui <lauran.cui@gmail.com>
2016-09-05 00:21:21 -0700

    web pack bundle added

--- dispatcher/LocDispatcher.js
+++
@@ -1,9 +0,0 @@
-var Dispatcher = require('flux').Dispatcher;
-var locDispatcher = new Dispatcher();
-
-locDispatcher.handleViewAction = function(action){
-	this.dispatch({
-		source: "View_Action",
-		action: action
-	});
-}
